c9ed1337a0e48b136e099209b86d0dc61581f15e
Ben Altman <altmanguitar@gmail.com>
2020-09-04 23:11:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.collect {|name| badges << badge_maker(name)}
+  badges
+end
+
+def assign_rooms(attendees)
+
+end
