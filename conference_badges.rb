1cbd83910d4d757921f516e1f3373cfe0dfdcce7
Ben Altman <altmanguitar@gmail.com>
2020-09-05 00:16:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,24 @@
 require 'pry'
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  return "Hello, my name is #{name}." #returns label text
 end
 
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.collect {|name| badges << badge_maker(name)}
-  badges
+  badges = [] #start by creating a new array to hold the results of the operation
+  attendees.collect {|name| badges << badge_maker(name)} #collect new values then push to new array
+  badges #return result
 end
 
 def assign_rooms(attendees)
-  rooms = []
-  attendees.each_with_index {|speaker, index| rooms << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
-  rooms
+  rooms = [] #again, start with new array to hold the new values
+  attendees.each_with_index {|speaker, index| rooms << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"} #creat two new variables, remember index starts at 0
+  rooms #return result
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |sticker|
-    puts sticker
+  batch_badge_creator(attendees).each do |sticker| #call method and operate on output values
+    puts sticker #puts the result
   end
 
   assign_rooms(attendees).each do |assignment|
